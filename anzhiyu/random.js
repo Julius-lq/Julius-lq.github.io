a13f57e9a81e265f0a842adf2926be97b2fd4c39
amoureux <2980395329@qq.com>
2024-04-24 22:58:51 +0800

    Site updated: 2024-04-24 22:58:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/20/hello-world/"];function toRandomPost(){
+var posts=["2024/04/20/Git统计项目代码量/","2024/04/20/Talk is cheap,show me the code/","2024/04/20/hello-world/","2024/04/20/text1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
